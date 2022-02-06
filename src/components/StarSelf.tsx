93351da0a44ceca120717a2517492b0f30f99a69
taurusboong2 <taurusboong2@gmail.com>
2022-02-06 16:21:00 +0900

    dont work useState object

--- src/components/StarSelf.tsx
+++ src/components/StarSelf.tsx
@@ -2,22 +2,36 @@ import React, { useState } from 'react';
 
 const StarSelf = () => {
   const [color, setColor] = useState({ color: 'red' });
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState({
+    star0: false,
+    star1: false,
+    star2: false,
+    star3: false,
+    star4: false,
+  });
 
-  const click = () => {
-    setToggle(!toggle);
+  const { star1, star2, star3, star4, star5 } = toggle;
+
+  const click = e => {
+    const starId = e.target.id;
+    const changedStar = toggle.starId;
+    console.log(setToggle(!star1));
+    setToggle({
+      ...toggle,
+      [starId]: !toggle,
+    });
     setColor(toggle ? { color: 'red' } : { color: 'green' });
+    console.log(starId);
+    console.log(toggle);
   };
 
   return (
     <div>
-      <h2 style={color} onClick={click}>
-        ★
-      </h2>
+      <h2>★</h2>
       <h1>별점 기능 연습장</h1>
       {[...Array(5)].map((e, i) => {
         return (
-          <span key={i} style={color} onClick={click}>
+          <span key={i} id={'star' + i} style={color} onClick={click}>
             ★
           </span>
         );
